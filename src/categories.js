8e663908c66a6d5a03ab3001a6a20164e64c4042
ZainabSb <zainabsbeity2003@gmail.com>
2022-08-19 21:59:22 +0300

    first commit

---
+++ src/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    {
+        name: "Women",
+        img: "/pics/cat/1.jpg",
+    },
+    {
+        name: "Kids",
+        img: "/pics/cat/2.jpg",
+    },
+
+    {   name: "Men",
+        img: "/pics/cat/3.jpg" 
+    },
+];
+
+export default categories;
